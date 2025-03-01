b7fdba9528d53f6b7b9806684e2ecd9f98738f02
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-03-02 02:14:57 +0500

    add auth repository

---
+++ lib/data/repositories/auth_repository.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  Future<User?> signUp(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<User?> login(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
